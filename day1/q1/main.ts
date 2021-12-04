94c75118320c47142e496df48792914a3924d34b
Whelch <blair.pearson@merrillcorp.com>
2021-12-04 11:49:42 -0600

    didn't mean to save this. .

--- day1/q1/main.ts
+++ day1/q1/main.ts
@@ -11,13 +11,3 @@ const result = chain(file)
   .valueOf();
 
 console.log(result);
-
-
-console.log(require('fs')
-  .readFileSync('./day1/input.txt')
-  .toString()
-  .split('\n')
-  .map((val: string) => +val)
-  .filter((val: number, index: number, list: number[]) => val > list[index-1])
-  .length
-)
