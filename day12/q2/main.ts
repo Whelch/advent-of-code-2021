ee22bfed48faae0400b6251776fad3d70e66ff3e
Whelch <blair.pearson@merrillcorp.com>
2021-12-11 23:59:23 -0600

    Day12

---
+++ day12/q2/main.ts
@@ -0,0 +1,56 @@
+import { readFileSync } from 'fs';
+import { chain, uniq, last, countBy } from 'lodash';
+
+const file = readFileSync('./day12/input.txt');
+
+let roomConnections = chain(file)
+  .split('\n')
+  .map(line => line.split('-'))
+  .valueOf();
+
+const rooms: Record<string, string[]> = chain(roomConnections)
+  .flatten()
+  .uniq()
+  .reduce((agg: Record<string, string[]>, room: string) => ({
+    ...agg,
+    [room]: chain(roomConnections)
+      .filter(connection => connection.includes(room))
+      .flatten()
+      .without(room, 'start')
+      .valueOf(),
+  }), {} as Record<string, string[]>)
+  .valueOf();
+
+function isBigRoom(roomName: string): boolean {
+  return roomName === roomName.toUpperCase();
+}
+
+function validAdditionToPath(path: string[], newRoom: string): boolean {
+  const noDupeSmallRoom = chain(path)
+    .reject(isBigRoom)
+    .countBy()
+    .values()
+    .every(count => count === 1)
+    .valueOf();
+
+  return isBigRoom(newRoom) || path.every(room => room !== newRoom) || noDupeSmallRoom;
+}
+
+const unfinishedPaths: string[][] = [['start']];
+let numberOfPaths = 0;
+
+while(unfinishedPaths.length > 0) {
+  const path = unfinishedPaths.pop();
+
+  rooms[last(path)].forEach(connectedRoom => {
+    if (validAdditionToPath(path, connectedRoom)) {
+      if (connectedRoom === 'end') {
+        numberOfPaths++;
+      } else {
+        unfinishedPaths.push([...path, connectedRoom]);
+      }
+    }
+  })
+}
+
+console.log(numberOfPaths);
